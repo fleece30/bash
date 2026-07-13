07d7e2f94b08576b7aa965b292e5faf2843ff036
fleece30 <abhiyad25@gmail.com>
2026-07-12 22:44:23 -0700

    fix: update to add nvim install directory to path

--- install-nvim.sh
+++ install-nvim.sh
@@ -21,6 +21,12 @@ mv "nvim-linux-${NVIM_ARCH}" "$INSTALL_DIR"
 rm "nvim-linux-${NVIM_ARCH}.tar.gz"
 ln -sf "$INSTALL_DIR/bin/nvim" /usr/local/bin/nvim
 
+if ! echo "$PATH" | grep -q "/usr/local/bin"; then
+  echo "==> /usr/local/bin missing from PATH, adding it system-wide..."
+  echo 'export PATH="$PATH:/usr/local/bin"' > /etc/profile.d/local-bin.sh
+  chmod +x /etc/profile.d/local-bin.sh
+fi
+
 echo "==> Installing dependencies"
 apt update -qq
 apt install -y git curl unzip build-essential ripgrep fd-find
